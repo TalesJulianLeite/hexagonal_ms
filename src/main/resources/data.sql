f58bb1b685fc0fc3b4d240a7d87870ac1bd0cbb9
Tales Julian Leite <talesjulianl@gmail.com>
2024-08-22 08:54:00 -0300

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,1 @@
-INSERT INTO USUARIO (cod_usuario, nome_usuario, password, unidade, fun_ativo) VALUES (1, 'tales.leite', "tale@2024", "52241635000184", "S");
+INSERT INTO USUARIO (cod_usuario, nome_usuario, password, unidade, fun_ativo) VALUES (1, 'tales.leite', "admin123", "1", "S");
